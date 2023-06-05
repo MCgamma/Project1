f430173e9d6018cd581ffc302a95429fe9daad65
MCgamma <simon.terti@gmail.com>
2023-06-05 08:36:48 +0300

    set wiki/name

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 
 from . import util
+import markdown2
+from markdown2 import markdown_path
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def title(request, name):      # There is a problem with capital letters
+
+    if name in util.list_entries():
+        file = markdown2.markdown(markdown_path(f"./entries/{name}.md"))
+        return render(request, "encyclopedia/exist.html", {
+            "name":name, "file":file
+        })
+
+    else:
+        return render(request, "encyclopedia/exist.html", {
+            "name": name
+        } )
+        
+
 
