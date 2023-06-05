48f7f6d6e50c9a99f2e25da3dbcbba075ddbec93
MCgamma <simon.terti@gmail.com>
2023-06-06 00:26:48 +0300

    Fix capital bug
    Fix the capitalize bug. You can search with capital or lowercase now

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -10,18 +10,19 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def title(request, name):      # There is a problem with capital letters
+def title(request, name):      
 
-    if name in util.list_entries():
+    if name.casefold() in (x.casefold() for x in util.list_entries()):
+        lower = [y.casefold() for y in util.list_entries()] 
+        a = lower.index(name.casefold())
+        normal = util.list_entries()
         file = markdown2.markdown(markdown_path(f"./entries/{name}.md"))
         return render(request, "encyclopedia/exist.html", {
-            "name":name, "file":file
+            "name":normal[a], "file":file
         })
 
     else:
         return render(request, "encyclopedia/exist.html", {
             "name": name
         } )
-        
-
 
