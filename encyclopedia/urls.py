b85a60af7f047799d30b546d3ac390d1e472a6e8
MCgamma <simon.terti@gmail.com>
2023-06-06 01:57:00 +0300

    add app_name encyclo

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclo"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:name>", views.title, name="title"),
