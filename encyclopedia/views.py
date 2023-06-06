a2466d2921f7f1b6700a128146b39f5197d7b225
MCgamma <simon.terti@gmail.com>
2023-06-06 05:38:01 +0300

    entry search (3.1 bullet)

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,24 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from django.http import HttpResponse
 from . import util
 import markdown2
 from markdown2 import markdown_path
+from django import forms
+
+class SearchForm(forms.Form):
+    search = forms.CharField(label="Search Encyclopedia")
+
+lower = [y.casefold() for y in util.list_entries()] 
+normal = util.list_entries()
 
 
 def index(request):
+
+    if request.method == "POST":
+        sform = SearchForm(request.POST) 
+        if sform.is_valid():
+            a = sform.cleaned_data["search"]
+            if a.casefold() in lower:
+                           
+                return redirect('encyclo:title', name = a)
+            else:
+                return HttpResponse("Hello, Simao")
+        
+        
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(), "form":SearchForm
     })
+
 
 def title(request, name):      
 
     if name.casefold() in (x.casefold() for x in util.list_entries()):
-        lower = [y.casefold() for y in util.list_entries()] 
-        a = lower.index(name.casefold())
-        normal = util.list_entries()
+        b = lower.index(name.casefold())
         file = markdown2.markdown(markdown_path(f"./entries/{name}.md"))
         return render(request, "encyclopedia/exist.html", {
-            "name":normal[a], "file":file
+            "name":normal[b], "file":file
         })
 
     else:
